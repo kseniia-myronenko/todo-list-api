c99c0858518a0ffa2ef4a279d8fc4758fc853397
Kseniia Myronenko <kseniia_myronenko@epam.com>
2022-07-13 15:29:56 +0300

    Define models.

---
+++ db/migrate/20220713121655_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects, id: :uuid do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
